bc9577d5783a4e55fd2df9dd57dee887a9c48eda
mirchai <EPOST@EPOST.EPOST>
2023-03-08 19:01:55 +0100

    Opprettet OUs

---
+++ all_Scripts/1_createOUs.ps1
@@ -0,0 +1,27 @@
+# Oprett OU-struktur i AD for brukere, maskinger og grupper
+
+# OU=Casca,DC=casca,DC=com
+
+New-ADOrganizationalUnit "Casca" -Description "Casca OU"
+
+
+# Opprette en OU-struktur for bedriften Casca
+# Vi har følgende OU'er - Casca_Users, Casca_Groups og Casca_Computers
+$users = "Casca_Users"
+$groups = "Casca_Groups"
+$computers = "Casca_Computers"         
+
+$topOUs = @($users,$groups,$computers)
+$depts = @('finance', 'hr', 'consultants', 'marketing') # må se hva vi gjør med IT og cyber security under consultants
+
+
+# Oppretter alle OUene
+foreach ($ou in $topOUs) {
+    New-ADOrganizationalUnit $ou `
+    -ProtectedFromAccidentalDeletion:$false `
+    -Path "OU=Casca,DC=casca,DC=com" `
+    -Description "Top OU for Casca" `
+}
+
+#liste ut alt
+Get-ADOrganizationalUnit -Filter * | Select-Object name
