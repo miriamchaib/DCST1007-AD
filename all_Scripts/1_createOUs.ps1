fe736dae69b3879c3298cc68599ada44b9332a0c
mirchai <EPOST@EPOST.EPOST>
2023-03-08 21:23:17 +0100

    Opprettet security groups ( local and global)

--- all_Scripts/1_createOUs.ps1
+++ all_Scripts/1_createOUs.ps1
@@ -30,9 +30,48 @@ foreach ($ou in $topOUs) {
         -Path $topOU.DistinguishedName  `
         -Description "Department OU for $dept in topOU $depts" `
         -ProtectedFromAccidentalDeletion:$false
-    }
 
+    }
 }
+
 
 #liste ut alt
 Get-ADOrganizationalUnit -Filter * | Select-Object name
+
+
+# CREATE GROUPS
+
+# Etter å ha opprettet OUene er det på tide å opprette gruppe-objektene, deretter skal vi lage brukere
+# fil nr 2
+
+# når vi har laget brukere skal vi bruke den koden til å lage admin brukere ( en admin er en bruker først, og deretter legger vi til rettighetene)
+
+
+# for each object inside the groups array, create the OU with the path and description where the description is Groups/$
+# security groups
+$groups = @('local', 'global')
+$groups | ForEach-Object {
+    New-ADOrganizationalUnit $_ -Path "OU=Groups,OU=Casca,DC=casca,DC=com" -Description "$_OU group"
+}
+
+# $depts = @('finance', 'hr', 'consultants', 'marketing')
+
+
+foreach ($dept in $depts) {
+
+    $local_Groups = "l_$_"
+    $global_Groups = "g_$_"
+
+    New-ADGroup -Name $local_Groups `
+    -GroupScope Global `
+    -GroupCategory Security `
+    -Path "OU=local,OU=Groups,OU=Casca,DC=casca,DC=com" `
+    -Description "Local Group for $_"
+
+    # Create the global group in the Groups\global OU
+    New-ADGroup -Name $global_Groups `
+    -GroupScope Global `
+    -GroupCategory Security `
+    -Path "OU=global,OU=Groups,OU=Casca,DC=casca,DC=com" `
+    -Description "Global Group for $_"
+}
