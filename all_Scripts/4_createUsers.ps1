6546d15933b3949c391114a0c768e45bbd1918cc
mirchai <EPOST@EPOST.EPOST>
2023-03-09 13:20:30 +0100

    pusher kode fra forelesning midlertidig

--- all_Scripts/4_createUsers.ps1
+++ all_Scripts/4_createUsers.ps1
@@ -1,56 +1,37 @@
-
-# Hard way - Brukere MED særnorske tegn og vasking/formatering av data, randomgenerering av passord #
-New-Item -Path '.\Documents' -ItemType Directory
-
-$file = '.\users_advanced.csv'
-$localpath = '.\Documents/' #<--Kan brukes ved import csv
-#$session = New-PSSession -ComputerName 'dc1'
-Copy-Item -Path $file -Destination $localpath  # <-- må kjøres fra lokal powershell, ikke tilkoblet DC1
-
-$csvpath = $localpath + "users_advanced.csv"
-$users = Import-Csv -Path $csvpath -Delimiter ";"
-
-
-#Eksporterer brukere og passord som blir generert.
-#Ikke beste praksis med passord i klartekst og fil, men vi trenger å logge på brukerne etterpå
-$exportpath = $localpath + "export_users.csv"
-$exportpathfinal = $localpath + "export_users_final.csv"
-$csvfile = @()
-
-function New-Password 
-{
-  param(
-    [int]$Length = 12
-  )
-
-  $characterSet = 37..126 -as [char[]]
-  $password = ''
-  1..$Length | ForEach-Object {
-    $password += $characterSet | Get-Random
-  }
-
-  return $password
-}
-$password = New-Password
-
-
-Function Get-UserPrincipalName {
-    Param(
-        [Parameter(Mandatory=$True)][string] $fornavn,
-        [Parameter(Mandatory=$True)][string] $etternavn
+$users = Import-Csv -Path 'users.csv' -Delimiter ";"
+$csvfile  = @()
+$exportuserspath = 'export_users.csv'
+Function New-UserPassword {
+    $chars = [char[]](
+        (33..47 | ForEach-Object {[char]$_}) +
+        (58..64 | ForEach-Object {[char]$_}) +
+        (91..96 | ForEach-Object {[char]$_}) +
+        (123..126 | ForEach-Object {[char]$_}) +
+        (48..57 | ForEach-Object {[char]$_}) +
+        (65..90 | ForEach-Object {[char]$_}) +
+        (97..122 | ForEach-Object {[char]$_})
     )
 
-    If ( $fornavn -match $([char]32) ) {
-	    
+    -join (0..14 | ForEach-Object { $chars | Get-Random })
+}
+
+#Funksjon for å formatere csv-fil data til ønsket og godtatt format
+function New-UserInfo {
+    param (
+        [Parameter(Mandatory=$true)][string] $fornavn,
+        [Parameter(Mandatory=$true)][string] $etternavn
+    )
+
+    if ($fornavn -match $([char]32)) {
         $oppdelt = $fornavn.Split($([char]32))
         $fornavn = $oppdelt[0]
-        For ( $index = 1 ; $index -lt $oppdelt.Length ; $index ++ ) {
+
+        for ( $index = 1 ; $index -lt $oppdelt.Length ; $index ++ ) {
             $fornavn += ".$($oppdelt[$index][0])"
         }
-    } 
+    }
 
     $UserPrincipalName = $("$($fornavn).$($etternavn)").ToLower()
-
     $UserPrincipalName = $UserPrincipalName.Replace('æ','e')
     $UserPrincipalName = $UserPrincipalName.Replace('ø','o')
     $UserPrincipalName = $UserPrincipalName.Replace('å','a')
@@ -59,32 +40,16 @@ Function Get-UserPrincipalName {
     Return $UserPrincipalName
 }
 
-ForEach ( $user in $users ) {
-    $password = New-Password
-    $line = New-Object -TypeName PSObject
+foreach ($user in $users) {
+    $password = New-UserPassword
+    $line = New-Object -TypeName psobject
 
-    Add-Member -InputObject $line -MemberType NoteProperty -Name GivenName -Value $user.GivenName
+    Add-Member -InputObject $line -MemberType NoteProperty -Name GivenName -Value $User.GivenName
     Add-Member -InputObject $line -MemberType NoteProperty -Name SurName -Value $user.SurName
-    Add-Member -InputObject $line -MemberType NoteProperty -Name UserPrincipalName -Value `
-	                  "$(Get-UserPrincipalName -Fornavn $user.GivenName -Etternavn $user.SurName)@casca.com"
-    Add-Member -InputObject $line -MemberType NoteProperty -Name DisplayName -Value "$($user.GivenName) $($user.SurName)"
-    Add-Member -InputObject $line -MemberType NoteProperty -Name Department -Value $user.Department
+    Add-Member -InputObject $line -MemberType NoteProperty -Name UserPrincipalName -Value "$(New-UserInfo -Fornavn $user.GivenName -Etternavn $user.SurName)@core.sec"
+    Add-Member -InputObject $line -MemberType NoteProperty -Name DisplayName -Value "$($user.GivenName) $($user.surname)" 
+    Add-Member -InputObject $line -MemberType NoteProperty -Name department -Value $user.Department
     Add-Member -InputObject $line -MemberType NoteProperty -Name Password -Value $password
     $csvfile += $line
-}
-$csvfile | Export-Csv -Path $exportpath -NoTypeInformation -Encoding 'UTF8'
-Import-CSV -Path $exportpath | ConvertTo-CSV -NoTypeInformation | Out-File $exportpathfinal -Encoding utf8
-
-$users = Import-CSV -Path $exportpathfinal -Delimiter ","
-$sam=@()
-foreach ($user in $users) {
-    $sam = $User.UserPrincipalName.Split("@")
-        if ($sam[0].Length -gt 15) {
-            "for lang"
-            $sam[0] = $sam[0].Substring(0, 15)
-        }
-		'******'
-    $sam[0]
-    [string]$samaccountname = $sam[0]  
 }
 
